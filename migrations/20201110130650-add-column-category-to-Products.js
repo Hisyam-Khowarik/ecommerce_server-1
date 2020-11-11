eacea6d487a315695dab5f0f85a1a6e552878474
Hisyam-Khowarik <hisyamkhowarik@gmail.com>
2020-11-12 00:46:47 +0700

    1. TDD Jest Done 2. Authentication dan Authorization Done 3. Error Handler Done 4. Api Documentation Done 5. CRUD Server Done 6. Login Server Done

---
+++ migrations/20201110130650-add-column-category-to-Products.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn(
+      'Products', 
+      'category',
+      {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Products', 'category')
+  }
+};
